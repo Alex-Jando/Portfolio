2e071e86062cc0b2122aec50368a833e4030cc5a
Alex Jando <alexjando2007@outlook.com>
2026-01-28 09:48:50 -0500

    Fixed typo in sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -23,7 +23,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1.0,
     },
     {
-      url: "https://jando.live/collections",
+      url: "https://jando.live/collection",
       priority: 0.6,
     },
   ];
